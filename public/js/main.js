bfea6dd9d5f043ad177e06c3a29b3e2d442c719a
Blazexcalibur <sonicspeeds@gmail.com>
2016-08-17 11:49:13 -0400

    add jquery to post audio buffer

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,13 @@
   // Your custom JavaScript goes here
 
 
-
-})();
+    $('#stop-recording-audio').click( function(event){
+      console.log("jquery", buffer)
+      $.post("/recognize",
+      {
+        buffer: buffer
+      }, function(data, status){
+              alert("Data: " + data + "\nStatus: " + status);
+          })
+    })
+});
